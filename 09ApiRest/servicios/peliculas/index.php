bd73c35678b5339b71e520a4954b2bf9dc267f1c
Alejandro <alonsoalex_1@hotmail.com>
2024-10-15 08:07:28 -0600

    patch y delete en apirest

--- 09ApiRest/servicios/peliculas/index.php
+++ 09ApiRest/servicios/peliculas/index.php
@@ -8,7 +8,12 @@ switch($_SERVER['REQUEST_METHOD']){
 
         $conn = conexion($conexion);
         $res = getPeliculas($conn);
-        echo $res;
+        $array = array();
+        $array['status'] = 200;
+        $array['error'] = "false";
+        $array['data'] = $res;
+        $array = json_encode($array, UTF8);
+        echo $array;
         break;
     case 'POST':
         if($data = json_decode(file_get_contents("php://input"))) { 
@@ -25,8 +30,38 @@ switch($_SERVER['REQUEST_METHOD']){
             echo $array;
             die();
         }
+    case 'PATCH':
+        if($data = json_decode(file_get_contents("php://input"))) { 
+            $conn = conexion($conexion);
+            $res = patchPeliculas($conn);
+            echo $res;
+            return $res;
+        } else {
+            $array = array();
+            $array['estatus']	=	400;
+            $array['error']   	=	"Error de datos";
+            $array=json_encode($array);
+            echo $array;
+            die();
+        }
+    case 'DELETE':
+        if($data = json_decode(file_get_contents("php://input"))) { 
+            $conn = conexion($conexion);
+            $res = deletePeliculas($conn);
+            echo $res;
+            return $res;
+        } else {
+            $array = array();
+            $array['estatus']	=	400;
+            $array['error']   	=	"Error de datos";
+            $array=json_encode($array);
+            echo $array;
+            die();
+        }
+
     default:
         break;
+    
 }
 
 closeConn($conn, $conexion);
--- 09ApiRest/serviciosDatos/peliculas/peliculas.php
+++ 09ApiRest/serviciosDatos/peliculas/peliculas.php
@@ -3,7 +3,7 @@
 
 function getPeliculas($conn){
 
-    $sql = "SELECT * FROM pelicula";
+    $sql = "SELECT * FROM pelicula WHERE estado = 1";
     $result = $conn->query($sql);
     $row    = $result->fetch_all();
     $row    = json_encode($row,UTF8);
@@ -35,3 +35,65 @@ function postPeliculas($conn){
         }
     }
 }
+
+
+function patchPeliculas($conn) {
+    if ($_SERVER['REQUEST_METHOD'] === 'PATCH') {
+        $data = json_decode(file_get_contents("php://input"), true);
+        if (!empty($data['id'])) {
+            $id = $conn->real_escape_string($data['id']); 
+            $fields = [];
+            if (!empty($data['nombre'])) {
+                $nombre = $conn->real_escape_string($data['nombre']);
+                $fields[] = "nombre = '$nombre'";
+            }
+            if (!empty($data['publicado'])) {
+                $publicado = $conn->real_escape_string($data['publicado']);
+                $fields[] = "publicado = '$publicado'";
+            }
+            if (!empty($data['director'])) {
+                $director = $conn->real_escape_string($data['director']);
+                $fields[] = "director = '$director'";
+            }
+            if (!empty($data['portada'])) {
+                $portada = $conn->real_escape_string($data['portada']);
+                $fields[] = "portada = '$portada'";
+            }
+            $fecha_modificacion = date('Y-m-d H:i:s'); 
+            $fields[] = "fecha_modificacion = '$fecha_modificacion'";
+            if (count($fields) > 0) {
+                $sql = "UPDATE pelicula SET " . implode(", ", $fields) . " WHERE id = $id";
+                if ($conn->query($sql) === TRUE) {
+                    echo json_encode(["message" => "Película modificada correctamente"]);
+                } else {
+                    echo json_encode(["error" => "Error al modificar la película: " . $conn->error]);
+                }
+            } else {
+                echo json_encode(["error" => "No se enviaron datos para actualizar."]);
+            }
+        } else {
+            echo json_encode(["error" => "ID de película no proporcionado."]);
+        }
+    }
+}
+
+function deletePeliculas($conn) {
+    if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if (!empty($data['id'])) {
+            $id = $conn->real_escape_string($data['id']); 
+
+            $sql = "UPDATE pelicula SET estado = 0 WHERE id = $id";
+
+            if ($conn->query($sql) === TRUE) {
+                echo json_encode(["message" => "Película eliminada correctamente"]);
+            } else {
+                echo json_encode(["error" => "Error al eliminar la película: " . $conn->error]);
+            }
+        } else {
+            echo json_encode(["error" => "ID de película no proporcionado."]);
+        }
+    }
+}
+
