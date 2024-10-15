e151bf2083033a918f28301809de99bed8d2f334
Alejandro Alonso <alonsoalex_1@hotmail.com>
2024-10-14 22:05:06 -0600

    Post Peliculas en 09ApiRestPHP

--- 09ApiRest/servicios/peliculas/index.php
+++ 09ApiRest/servicios/peliculas/index.php
@@ -1,42 +1,22 @@
 <?php
 
-// include "../../base/MySQL.php";
-
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "peliculas";
-// $conn=conexion($servername, $username, $password, $dbname);
-
-
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-// $conn->set_charset("utf8mb4");
-
-// $sql = "SELECT * FROM pelicula";
-
-// $result = $conn->query($sql);
-// $row = $result->fetch_all();
-// $row =  json_encode($row,UTF8);
-// $conn->close();
-// echo $row;
-
-
-
 $config = "peliculas/configPeliculas.php";
 include_once "../../base/encabezado.php";
 
 switch($_SERVER['REQUEST_METHOD']){
     case 'GET':
+
         $conn = conexion($conexion);
         $res = getPeliculas($conn);
         echo $res;
         break;
     case 'POST':
         if($data = json_decode(file_get_contents("php://input"))) { 
-            return $data;
+
+            $conn = conexion($conexion);
+            $res = postPeliculas($conn);
+            echo $res;
+            return $res;
         } else {
             $array = array();
             $array['estatus']	=	400;
@@ -45,7 +25,6 @@ switch($_SERVER['REQUEST_METHOD']){
             echo $array;
             die();
         }
-        // break;
     default:
         break;
 }
--- 09ApiRest/serviciosDatos/peliculas/peliculas.php
+++ 09ApiRest/serviciosDatos/peliculas/peliculas.php
@@ -12,7 +12,26 @@ function getPeliculas($conn){
 }
 
 function postPeliculas($conn){
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = json_decode(file_get_contents("php://input"), true);
+        if (!empty($data['nombre']) && !empty($data['publicado']) && !empty($data['director']) && !empty($data['portada'])) {
 
+            
+            $nombre = $conn->real_escape_string($data['nombre']);
+            $publicado = $conn->real_escape_string($data['publicado']); 
+            $director = $conn->real_escape_string($data['director']);
+            $portada = $conn->real_escape_string($data['portada']);
 
+            $sql = "INSERT INTO pelicula (nombre, publicado, director, portada) 
+                    VALUES ('$nombre', '$publicado', '$director', '$portada')";
 
+            if ($conn->query($sql) === TRUE) {
+                echo json_encode(["message" => "Película insertada correctamente"]);
+            } else {
+                echo json_encode(["error" => "Error al insertar la película: " . $conn->error]);
+            }
+        } else {
+            echo json_encode(["error" => "Datos incompletos. Faltan campos requeridos."]);
+        }
+    }
 }
